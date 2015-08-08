350a476b06e1f415b661a2ef9a42a149efa2abdf
roof <roof@odd-e.com>
2015-08-08 10:40:52 +0700

    test fizzbuzz count number 1-5 correct answer

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,7 @@
 class FizzBuzz:
 	def count(self,number):
+		if number == 3:
+			return "fizz"
+		if number == 5:
+			return "buzz"
 		return str(number)
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -8,3 +8,10 @@ class FizzBuzzTest(unittest.TestCase):
 	def test_it_should_be_default_value(self):
 		self.assertEqual("1",self.fizzbuzz.count(1))
 		self.assertEqual("2",self.fizzbuzz.count(2))
+		self.assertEqual("4",self.fizzbuzz.count(4))
+
+	def test_it_should_be_fizz(self):
+		self.assertEqual("fizz",self.fizzbuzz.count(3))
+
+	def test_it_should_be_buzz(self):
+		self.assertEqual("buzz",self.fizzbuzz.count(5))
