55ec3ae718b2c855ac54caa29338b4ae6db4f498
roof <roof@odd-e.com>
2015-08-08 10:21:08 +0700

    test fizzbuzz default value 1 succeed

---
+++ fizzbuzz.py
@@ -0,0 +1,6 @@
+class FizzBuzz:
+	def __init__(self,number):
+		self.number = number
+
+	def count(self):
+		return "1"
---
+++ test_fizzbuzz.py
@@ -0,0 +1,7 @@
+import unittest
+from fizzbuzz import FizzBuzz
+
+class FizzBuzzTest(unittest.TestCase):
+	def test_it_should_be_default_value(self):
+		fizzbuzz = FizzBuzz(1)
+		self.assertEqual("1",fizzbuzz.count())
