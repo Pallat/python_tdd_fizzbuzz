c6fc1827ac2b2b2e65fb9b6bd5b50d0d146feeb1
roof <roof@odd-e.com>
2015-08-08 10:44:05 +0700

    test to fizzbuzz correct

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
 class FizzBuzz:
 	def count(self,number):
-		if number == 3:
+		if number == 15:
+			return "fizzbuzz"
+		if number%3 == 0:
 			return "fizz"
-		if number == 5:
+		if number%5 == 0:
 			return "buzz"
 		return str(number)
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -9,9 +9,17 @@ class FizzBuzzTest(unittest.TestCase):
 		self.assertEqual("1",self.fizzbuzz.count(1))
 		self.assertEqual("2",self.fizzbuzz.count(2))
 		self.assertEqual("4",self.fizzbuzz.count(4))
+		self.assertEqual("7",self.fizzbuzz.count(7))
+		self.assertEqual("8",self.fizzbuzz.count(8))
 
 	def test_it_should_be_fizz(self):
 		self.assertEqual("fizz",self.fizzbuzz.count(3))
+		self.assertEqual("fizz",self.fizzbuzz.count(6))
+		self.assertEqual("fizz",self.fizzbuzz.count(9))
 
 	def test_it_should_be_buzz(self):
 		self.assertEqual("buzz",self.fizzbuzz.count(5))
+		self.assertEqual("buzz",self.fizzbuzz.count(10))
+
+	def test_it_should_be_fizzbuzz(self):
+		self.assertEqual("fizzbuzz",self.fizzbuzz.count(15))
