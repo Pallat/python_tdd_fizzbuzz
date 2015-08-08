7ebb8a9b8aaa1e5776d504b4eea42bac2b27c8bb
roof <roof@odd-e.com>
2015-08-08 10:26:06 +0700

    refactor to not use constructor

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,5 @@
 class FizzBuzz:
-	def __init__(self,number):
-		self.number = number
-
-	def count(self):
-		if (self.number == 2):
+	def count(self,number):
+		if number == 2:
 			return "2"
 		return "1"
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -3,7 +3,7 @@ from fizzbuzz import FizzBuzz
 
 class FizzBuzzTest(unittest.TestCase):
 	def test_it_should_be_default_value(self):
-		fizzbuzz = FizzBuzz(1)
-		self.assertEqual("1",fizzbuzz.count())
-		fizzbuzz = FizzBuzz(2)
-		self.assertEqual("2",fizzbuzz.count())
+		fizzbuzz = FizzBuzz()
+		self.assertEqual("1",fizzbuzz.count(1))
+		fizzbuzz = FizzBuzz()
+		self.assertEqual("2",fizzbuzz.count(2))
